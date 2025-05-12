3eafae85bb15a785c187cda58eaca693b4068247
tomcruzana <tomcruzana@ymail.com>
2025-05-11 17:19:52 -0700

    added ingredient list return data

--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -10,6 +10,26 @@ export default function IngredientsList(props) {
     </div>
   ));
   return (
-    
+    <section>
+      <h2>Ingredients on hand:</h2>
+      <ul className="ingredients-list" aria-live="polite">
+        {ingredientsListItems}
+      </ul>
+
+      {props.ingredients.length > 3 && (
+        <div className="get-recipe-container">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button onClick={props.getRecipe}>
+            <span>
+              <i class="icon-robot"></i>
+            </span>{" "}
+            Generate recipe
+          </button>
+        </div>
+      )}
+    </section>
   );
 }
