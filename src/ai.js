c0b25732f439e03020f670f1c63ba28828743f5d
tomcruzana <tomcruzana@ymail.com>
2025-05-11 17:23:39 -0700

    added a comment in mystral ai script

--- src/ai.js
+++ src/ai.js
@@ -4,6 +4,7 @@ const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
 `;
 
+// Your hugging face Mystal public API key goes here
 const hf = new HfInference('YOUR_HUGGING_FACE_PUBLIC_API_KEY');
 
 export async function getRecipeFromMistral(ingredientsArr) {
